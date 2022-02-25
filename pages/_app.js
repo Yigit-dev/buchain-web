460fa9e979c43454086e89defb8ab4effa44ff0e
Lamia <lamiademirok@gmail.com>
2022-02-25 17:02:23 +0300

    Icon component created
    -Added icons, favicon and edited Buchain logo
    -Created icon component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import { NextUIProvider } from '@nextui-org/react'
 import { theme } from '../src/utils/theme'
+import "../styles/globals.css";
+import "../styles/palette.module.scss"
+
 
 function MyApp({ Component, pageProps }) {
   return (
---
+++ src/components/base/icons.js
@@ -0,0 +1,27 @@
+import Envelope from '/public/icons/envelope.svg'
+import Twitter from '/public/icons/twitter.svg'
+import Instagram from '/public/icons/instagram.svg'
+import Discord from '/public/icons/discord.svg'
+import Hamburger from '/public/icons/hamburger.svg'
+import variables from '/styles/palette.module.scss'
+ 
+const iconStyle = {
+  height: '30px',
+  width: '30px',
+  fill: "#2a2d41",
+};
+
+const iconTypes = {
+  envelope: Envelope,
+  twitter: Twitter,
+  instagram: Instagram,
+  discord: Discord,
+  hamburger: Hamburger,
+};
+
+const IconComponent = ({ name, ...props }) => {
+  let Icon = iconTypes[name];
+  return <Icon {...props} style={iconStyle} />;
+};
+
+export default IconComponent;
