7432a8ee4581aa4fb8987bd76a4332875ca7f72a
berkingurcan <70534820+berkingurcan@users.noreply.github.com>
2022-02-27 21:17:45 +0300

    footer
    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import Socials from "./base/Socials"
+import { Text } from '@nextui-org/react'
+
+
+const Footer = () => {
+  return (
+    <>
+        <Socials />
+        <Text small>Copyright &copy; 2021 BUCHAIN</Text>
+    </>
+  )
+}
+
+export default Footer
