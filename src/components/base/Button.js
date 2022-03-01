556566b4f94c59b81af19006c3bfd7c1bc8153a8
berkingurcan <70534820+berkingurcan@users.noreply.github.com>
2022-03-01 20:18:57 +0300

    Update Button.js
    Button.courses

--- src/components/base/Button.js
+++ src/components/base/Button.js
@@ -11,3 +11,14 @@ Button.Register = function RegisterButton({children}) {
         <Button rounded>{children}</Button>
     )
 }
+
+Button.Courses = function CoursesButton({children}) {
+    return (
+        <Button.Group>
+            <Button light color="primary" auto>TÜMÜ</Button>
+            <Button light color="primary" auto>ŞİRKETLER İÇİN</Button>
+            <Button light color="primary" auto>ÜYELER İÇİN</Button>
+            <Button light color="primary" auto>YAZILIMCILAR İÇİN</Button>
+        </Button.Group>
+    )
+} 
