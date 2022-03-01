363d8a93e8a4cb0df25534519f79fc7fdd7a766c
berkingurcan <70534820+berkingurcan@users.noreply.github.com>
2022-03-01 20:09:37 +0300

    Update Socials.js

--- src/components/base/Socials.js
+++ src/components/base/Socials.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import { Grid } from '@nextui-org/react'
+import Icon from './Icon'
 
 const Socials = () => {
   return (
-    <div>
-      
-    </div>
+      <Grid.Container gap={2} justify="center">
+        <Grid>
+          <Icon name="instagram"/>
+        </Grid>
+        <Grid>
+          <Icon name="twitter" />
+        </Grid>
+        <Grid>
+          <Icon name="discord"/>
+        </Grid>
+      </Grid.Container>
   )
 }
 
