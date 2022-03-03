6a35d996d64f4ec895a483ac43c5a9c2d5d829e0
Yiğit Çakmak <yigitcakmak.dev@gmail.com>
2022-03-03 22:25:53 +0300

    Function Rename
    import problem

--- src/components/base/Button.js
+++ src/components/base/Button.js
@@ -1,23 +1,17 @@
 import { Button } from '@nextui-org/react'
 
-Button.Apply = function ApplyButton({children}) {
-    return(
-      <Button>{children}</Button>
-    )
+Button.Apply = function CustomButton({ children }) {
+  return <Button>{children}</Button>
 }
 
-Button.Register = function RegisterButton({children}) {
-    return(
-        <Button rounded>{children}</Button>
-    )
+Button.Register = function CustomButton({ children }) {
+  return <Button rounded>{children}</Button>
 }
 
-Button.Courses = function CoursesButton({children}) {
-    return (
-            <Button light color="primary" auto>{children}</Button>
-    )
-} 
-
-
-
-
+Button.Courses = function CustomButton({ children }) {
+  return (
+    <Button light color="primary" auto>
+      {children}
+    </Button>
+  )
+}
