b6ee3c75b5425db7ffb5d174e7a620a82fa42a1f
Lamia <lamiademirok@gmail.com>
2022-03-15 00:17:56 +0300

    Update Supporters.js

--- src/components/Supporters.js
+++ src/components/Supporters.js
@@ -5,28 +5,80 @@ import { Image } from '@nextui-org/react'
 import { Container, Row, Col } from '@nextui-org/react'
 import WordText from '/src/components/base/Text.js'
 
-const MainSupporter = () => {
+const Supporters = () => {
   return (
     <>
-        <Card hoverable style={{width:'60vw'}}>
-          <Grid.Container gap={2} 
-          display="flex"
-          justify="center" 
-          alignItems="center"
-          alignContent="center" >
-            <Grid xl={6}>
-              <Card.Image width={200} height={140} />
-            </Grid>
+          <div justify="center" align="center">
+            <Text.Title>X Destekçiler</Text.Title>
+          </div>
 
-            <Grid xl={6} >
-              <Text.Info>X ŞİRKETİ</Text.Info>
-              <p style={{ fontSize: '14px' }}>Şirket AçıklamasıŞirket AçıklamasıŞirket AçıklamasıŞirket AçıklamasıŞirket AçıklamasıŞirket AçıklamasıŞirket AçıklamasıŞirket AçıklamasıŞirket AçıklamasıŞirket AçıklamasıŞirket Açıklaması</p>
-            </Grid>
-          </Grid.Container>
-        </Card>
-        </>
-      )
-    } 
-    
-    export default MainSupporter
-    
+        <Grid.Container
+          gap={2}
+          justify="center"
+          display="flex"
+          alignItems="center"
+          alignContent="center"
+        >
+          <Grid xs={12} md={3} sm={3} justify="center" align="center">
+            <Card hoverable style={{ padding: '10px' }}>
+              <Card.Body>
+                <Card.Image width={200} height={140} />
+              </Card.Body>
+            </Card>
+          </Grid>
+
+          <Grid xs={12} md={3} sm={3}>
+            <Card hoverable style={{ padding: '10px' }}>
+              <Card.Body>
+                <Card.Image width={200} height={140} />
+              </Card.Body>
+            </Card>
+          </Grid>
+          <Grid xs={12} md={3} sm={3}>
+            <Card hoverable style={{ padding: '10px' }}>
+              <Card.Body>
+                <Card.Image width={200} height={140} />
+              </Card.Body>
+            </Card>
+          </Grid>
+        </Grid.Container>
+
+
+          <div justify="center" align="center">
+            <Text.Title>Y Destekçiler</Text.Title>
+          </div>
+
+      <Grid.Container
+        gap={2}
+        justify="center"
+        display="flex"
+        alignItems="center"
+        alignContent="center"
+      >
+        <Grid xs={12} md={3} sm={3} justify="center" align="center">
+          <Card hoverable style={{ padding: '10px' }}>
+            <Card.Body>
+              <Card.Image width={200} height={140} />
+            </Card.Body>
+          </Card>
+        </Grid>
+        <Grid xs={12} md={3} sm={3}>
+          <Card hoverable style={{ padding: '10px' }}>
+            <Card.Body>
+              <Card.Image width={200} height={140} />
+            </Card.Body>
+          </Card>
+        </Grid>
+        <Grid xs={12} md={3} sm={3}>
+          <Card hoverable style={{ padding: '10px' }}>
+            <Card.Body>
+              <Card.Image width={200} height={140} />
+            </Card.Body>
+          </Card>
+        </Grid>
+      </Grid.Container>
+    </>
+  )
+}
+
+export default Supporters
